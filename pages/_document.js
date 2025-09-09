bfcf9e496d012486f77a5fba4fa9e84d98514b5e
Joonas Jokinen <joonas@joonasjokinen.fi>
2025-09-09 14:42:55 +0300

    Add lang parameter to base <html> tag

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html>
+    <Html
+      lang="fi
+    "
+    >
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
