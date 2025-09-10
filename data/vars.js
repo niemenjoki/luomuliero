23eb7dffab6e58342306c9d2d11ae69e811f003c
Joonas Jokinen <joonas@joonasjokinen.fi>
2025-09-10 12:28:18 +0300

    Update the amount of blogposts visible per page

--- data/vars.js
+++ data/vars.js
@@ -1,4 +1,4 @@
-export const POSTS_PER_PAGE = 2;
+export const POSTS_PER_PAGE = 6;
 export const SITE_URL = 'https://luomuliero.fi';
 export const REPO_URL = 'https://github.com/niemenjoki/luomuliero';
 export const LICENSE_URL =
