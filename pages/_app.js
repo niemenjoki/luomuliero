286eb7c2cd8019d692352674739b554be4e0e4bd
Joonas Jokinen <joonas@joonasjokinen.fi>
2025-09-16 15:09:42 +0300

    migrate Rubik font to next/font with CSS variable for global usage; remove Google Fonts link from _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,24 @@ import '/styles/globals.css';
 import '/styles/highlight.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
+
+import { Rubik } from 'next/font/google';
+
 config.autoAddCss = false;
 
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['400', '700', '800'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
+
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={rubik.variable}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default App;
--- pages/_document.js
+++ pages/_document.js
@@ -1,22 +1,10 @@
+// pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html
-      lang="fi
-    "
-    >
+    <Html lang="fi">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,700;0,800;1,400;1,700;1,800&display=swap"
-          rel="stylesheet"
-        />
         <meta
           name="google-site-verification"
           content="QnhqvG850vkdtc4C7pk0jp9JUDowtwf-vVks_iHQLWY"
