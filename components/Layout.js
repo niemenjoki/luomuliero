5c424171f089e594bbc4caa588840a3bcf7a569e
Joonas Jokinen <joonas@joonasjokinen.fi>
2025-09-18 15:53:22 +0300

    Remove Google Analytics integration

--- components/Layout.js
+++ components/Layout.js
@@ -12,19 +12,6 @@ const Layout = ({ title, description, children, canonical }) => {
   const router = useRouter();
   const canonicalUrl = canonical || `${SITE_URL}${router.asPath}`;
 
-  const handleRouteChange = (url) => {
-    if (typeof window !== 'undefined' && window.gtag) {
-      window.gtag('config', 'G-0448XXF0S0', { page_path: url });
-    }
-  };
-
-  useEffect(() => {
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
-
   return (
     <div className="container">
       <Head>
@@ -47,23 +34,7 @@ const Layout = ({ title, description, children, canonical }) => {
         <link rel="shortcut icon" href="/icons/favicon.ico" />
         <meta name="theme-color" content="#0e111b" />
       </Head>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-0448XXF0S0"
-        strategy="afterInteractive"
-      />
-      <Script
-        id="analytics-loader"
-        dangerouslySetInnerHTML={{
-          __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-0448XXF0S0', { page_path: window.location.pathname });
-            `,
-        }}
-        strategy="afterInteractive"
-      />
+
       <Script
         async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
